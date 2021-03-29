ae4bcfc01e0dc81f41b24948824b350b59d85bc6
Aditya Oberai <adityaoberai1@gmail.com>
2021-03-29 13:24:02 +0530

    Added more hello world programs

---
+++ Example Hello World Apps/Go/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("!... Hello World ...!")
+	fmt.Println("The time now is ", time.Now())
+}
---
+++ Example Hello World Apps/Java/hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, World");
+        int num=23;
+        String name;
+        System.out.print("What is your name? ");
+        name=sc.nextLine();
+        System.out.println(name+" loves the number "+num);
+    }
+}
